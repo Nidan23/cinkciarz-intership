04eef3d0b9730b1f7cd631ea1795130c2b209459
Nidan23 <jakubsul123@gmail.com>
2021-11-16 12:52:51 +0100

    "First commit"

---
+++ src/main/java/first/project/uno/MappingController.java
@@ -0,0 +1,32 @@
+package first.project.uno;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MappingController {
+    private final CrudService crud;
+
+    public MappingController(CrudService crud){
+        this.crud = crud;
+    }
+
+    @PostMapping("/users")
+    public String addUser(@RequestBody UserEntity user){
+        crud.addUser(user);
+        return "\nUser added\n";
+    }
+
+    @PatchMapping(path = "/users/{id}", consumes = "application/json")
+    public String updateUser(@RequestBody UserEntity user, @PathVariable("id") Long id){
+        crud.updateUser(user, id);
+        return "\nUser updated\n";
+    }
+
+    @DeleteMapping(path = "/users/{id}")
+    public String deleteUser(@PathVariable("id") Long id) {
+        crud.deleteUser(id);
+        return "\nUser deleted\n";
+    }
+
+}
