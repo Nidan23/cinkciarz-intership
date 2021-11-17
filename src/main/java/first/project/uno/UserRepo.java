3fec4cb9b8cb56e5ade7e0c34132573b03767e27
Nidan23 <jakubsul123@gmail.com>
2021-11-17 10:20:07 +0100

    Init repo

--- src/main/java/first/project/uno/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package first.project.uno;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepo extends CrudRepository<UserEntity, Long> {
-}
